1bf6689017c8d7322583703d7d4a4cee2e5b964c
Dennis Labordus <dennis.labordus@alliander.com>
2021-12-07 12:16:55 +0100

    Some more tests.
    Signed-off-by: Dennis Labordus <dennis.labordus@alliander.com>

--- service/src/test/java/org/lfenergy/compas/scl/auto/alignment/common/CommonUtilTest.java
+++ service/src/test/java/org/lfenergy/compas/scl/auto/alignment/common/CommonUtilTest.java
@@ -14,7 +14,25 @@ class CommonUtilTest {
     }
 
     @Test
-    void cleanSXYDeclarationAndAttributes_WhenCalledWithSXYCoordinates_ThenDataRemoved() {
+    void cleanSXYDeclarationAndAttributes_WhenCalledWithSXYCoordinatesDefaultPrefix_ThenDataRemoved() {
+        var sclString = "<SCL xmlns:sxy=\"http://www.iec.ch/61850/2003/SCLcoordinates\">" +
+                "  <Bay sxy:x=\"1\" sxy:y=\"2\" name=\"BusBar B\" sxy:dir=\"horizontal\">" +
+                "    <ConnectivityNode pathName=\"AA1/J1/BusBar B/L1\" name=\"L1\"/>" +
+                "  </Bay>" +
+                "</SCL>";
+        var expectedString = "<SCL >" +
+                "  <Bay name=\"BusBar B\">" +
+                "    <ConnectivityNode pathName=\"AA1/J1/BusBar B/L1\" name=\"L1\"/>" +
+                "  </Bay>" +
+                "</SCL>";
+
+        var result = CommonUtil.cleanSXYDeclarationAndAttributes(sclString);
+        assertNotNull(result);
+        assertEquals(expectedString, result);
+    }
+
+    @Test
+    void cleanSXYDeclarationAndAttributes_WhenCalledWithSXYCoordinatesWithDifferentPrefix_ThenDataRemoved() {
         var sclString = "<SCL xmlns:somexy=\"http://www.iec.ch/61850/2003/SCLcoordinates\">" +
                 "  <Bay somexy:x=\"1\" somexy:y=\"2\" name=\"BusBar B\" somexy:dir=\"horizontal\">" +
                 "    <ConnectivityNode pathName=\"AA1/J1/BusBar B/L1\" name=\"L1\"/>" +
