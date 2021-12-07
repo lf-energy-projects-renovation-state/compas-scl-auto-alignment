aaa2591c109e6b4ca470037b598e91d752c9461d
Dennis Labordus <dennis.labordus@alliander.com>
2021-12-07 14:31:19 +0100

    Small changes for Native Image.
    Signed-off-by: Dennis Labordus <dennis.labordus@alliander.com>

--- app/src/main/java/org/lfenergy/compas/scl/auto/alignment/rest/CompasSclAutoAlignmentConfiguration.java
+++ app/src/main/java/org/lfenergy/compas/scl/auto/alignment/rest/CompasSclAutoAlignmentConfiguration.java
@@ -3,6 +3,7 @@
 // SPDX-License-Identifier: Apache-2.0
 package org.lfenergy.compas.scl.auto.alignment.rest;
 
+import io.quarkus.runtime.annotations.RegisterForReflection;
 import org.lfenergy.compas.core.commons.ElementConverter;
 
 import javax.enterprise.context.ApplicationScoped;
@@ -11,6 +12,8 @@ import javax.enterprise.inject.Produces;
 /**
  * Create Beans from other dependencies that are used in the application.
  */
+@RegisterForReflection(targets = {com.powsybl.sld.library.Components.class,
+        com.powsybl.sld.library.Component.class})
 public class CompasSclAutoAlignmentConfiguration {
     @Produces
     @ApplicationScoped
