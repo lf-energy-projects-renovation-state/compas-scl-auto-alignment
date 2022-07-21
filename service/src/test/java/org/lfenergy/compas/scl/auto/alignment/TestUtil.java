d5e22222bd7419d136f41086043b4636090726ba
Dennis Labordus <dennis.labordus@alliander.com>
2022-07-21 17:04:06 +0200

    Updated Test Data and first step to improved checks.
    Signed-off-by: Dennis Labordus <dennis.labordus@alliander.com>

--- service/src/test/java/org/lfenergy/compas/scl/auto/alignment/TestUtil.java
+++ service/src/test/java/org/lfenergy/compas/scl/auto/alignment/TestUtil.java
@@ -3,26 +3,36 @@
 // SPDX-License-Identifier: Apache-2.0
 package org.lfenergy.compas.scl.auto.alignment;
 
+import javanet.staxutils.SimpleNamespaceContext;
 import org.lfenergy.compas.core.commons.ElementConverter;
 import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
 
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathFactory;
 import java.io.*;
 import java.nio.charset.StandardCharsets;
 
-import static org.lfenergy.compas.scl.auto.alignment.SclAutoAlignmentConstants.SCL_ELEMENT_NAME;
-import static org.lfenergy.compas.scl.auto.alignment.SclAutoAlignmentConstants.SCL_NS_URI;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.lfenergy.compas.scl.auto.alignment.SclAutoAlignmentConstants.*;
 
 public final class TestUtil {
     private TestUtil() {
         // Only static methods.
     }
 
+    public static Element toElement(String sclData) throws IOException {
+        var converter = new ElementConverter();
+        try (var inputStream = new BufferedInputStream(
+                new ByteArrayInputStream(sclData.getBytes(StandardCharsets.UTF_8)))) {
+            return converter.convertToElement(inputStream, SCL_ELEMENT_NAME, SCL_NS_URI);
+        }
+    }
+
     public static Element readSCLElement(String filename) throws IOException {
         var sclData = readSCL(filename);
-        var converter = new ElementConverter();
-
-        return converter.convertToElement(new BufferedInputStream(
-                new ByteArrayInputStream(sclData.getBytes(StandardCharsets.UTF_8))), SCL_ELEMENT_NAME, SCL_NS_URI);
+        return toElement(sclData);
     }
 
     public static String readSCL(String filename) throws IOException {
@@ -30,13 +40,13 @@ public final class TestUtil {
     }
 
     public static String readFile(String filename) throws IOException {
-        var inputStream = TestUtil.class.getResourceAsStream(filename);
-        assert inputStream != null;
+        try (var inputStream = TestUtil.class.getResourceAsStream(filename)) {
+            assert inputStream != null;
 
-        return new String(inputStream.readAllBytes());
+            return new String(inputStream.readAllBytes());
+        }
     }
 
-    @Deprecated
     public static void writeFile(String fileName, String data) {
         var file = new File("target", fileName);
         try (var fw = new OutputStreamWriter(new FileOutputStream(file), StandardCharsets.UTF_8)) {
@@ -44,5 +54,21 @@ public final class TestUtil {
         } catch (IOException e) {
             throw new UncheckedIOException(e);
         }
+    }
+
+    public static void assertXYCoordinates(Element rootElement, String xPathExpression, Integer x, Integer y) throws Exception {
+        var xPath = XPathFactory.newInstance().newXPath();
+
+        SimpleNamespaceContext nsCtx = new SimpleNamespaceContext();
+        nsCtx.setPrefix("scl", SCL_NS_URI);
+        xPath.setNamespaceContext(nsCtx);
+
+        var nodeList = (NodeList) xPath.compile(xPathExpression).evaluate(rootElement.getOwnerDocument(), XPathConstants.NODESET);
+        assertEquals(1, nodeList.getLength());
+
+        var element = (Element) nodeList.item(0);
+        assertNotNull(element);
+        assertEquals(x.toString(), element.getAttributeNS(SCLXY_NS_URI, "x"));
+        assertEquals(y.toString(), element.getAttributeNS(SCLXY_NS_URI, "y"));
     }
 }
--- service/src/test/java/org/lfenergy/compas/scl/auto/alignment/service/SclAutoAlignmentEnricherTest.java
+++ service/src/test/java/org/lfenergy/compas/scl/auto/alignment/service/SclAutoAlignmentEnricherTest.java
@@ -17,12 +17,13 @@ import java.nio.charset.StandardCharsets;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.lfenergy.compas.scl.auto.alignment.SclAutoAlignmentConstants.SCL_ELEMENT_NAME;
 import static org.lfenergy.compas.scl.auto.alignment.SclAutoAlignmentConstants.SCL_NS_URI;
-import static org.lfenergy.compas.scl.auto.alignment.TestUtil.readFile;
-import static org.lfenergy.compas.scl.auto.alignment.TestUtil.readSCL;
+import static org.lfenergy.compas.scl.auto.alignment.TestUtil.*;
 
 class SclAutoAlignmentEnricherTest {
     @Test
-    void enrich_WhenPassingCase1_ThenXSDUpdated() throws IOException {
+    void enrich_WhenPassingCase1_ThenSCDUpdated() throws Exception {
+        // The JSON file used here is a copy created by the testcase
+        // SclAutoAlignmentServiceTest#createJson_WhenPassingCase1_ThenJsonReturned
         var filename = "scl-1";
         var jsonString = readFile("/json/" + filename + ".json");
         var scl = new GenericSCL(readSCLElement(filename + ".scd"));
@@ -32,10 +33,15 @@ class SclAutoAlignmentEnricherTest {
 
         assertNotNull(scl.getElement());
         writeFile(filename + "-updated.scd", scl);
+
+        assertXYCoordinates(scl.getElement(), "//scl:VoltageLevel[@name='J1']", 2, 4);
+        assertXYCoordinates(scl.getElement(), "//scl:Bay[@name='BusBar A']", 1, 15);
     }
 
     @Test
-    void enrich_WhenPassingCase2_ThenXSDUpdated() throws IOException {
+    void enrich_WhenPassingCase2_ThenSCDUpdated() throws IOException {
+        // The JSON file used here is a copy created by the testcase
+        // SclAutoAlignmentServiceTest#createJson_WhenPassingCase2_ThenJsonReturned
         var filename = "scl-2";
         var jsonString = readFile("/json/" + filename + ".json");
         var scl = new GenericSCL(readSCLElement(filename + ".scd"));
